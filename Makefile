eb361171bd4c4a851758c293ff27fd1d78dbff23
sadmonad <sadmonad@gmail.com>
2021-11-09 01:06:14 +0300

    Setup basic test environment

--- Makefile
+++ Makefile
@@ -1,10 +1,9 @@
 test:
 	pytest
 lint:
-	mypy src/
 	flake8
 	isort .
 run:
-	uvicorn src.main:app --reload
+	uvicorn ikigai.main:app --reload
 check:
 	curl localhost:8000/
--- tests/test_test.py
+++ tests/test_test.py
@@ -1,2 +1,12 @@
-def test_test():
-    assert 42 == 43
+import pytest
+from httpx import AsyncClient
+
+from ikigai.main import app
+
+
+@pytest.mark.anyio
+async def test_test():
+    async with AsyncClient(app=app, base_url='http://test') as ac:
+        response = await ac.get('/')
+    assert response.status_code == 200
+    assert response.json() == {'Hello': 'world'}
